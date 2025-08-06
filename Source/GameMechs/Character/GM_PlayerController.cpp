dffdbf3ffe4dae647f4884d13a3d397ab546d143
Nishant Verma <vnishant148@gmail.com>
2025-08-05 23:29:03 -0600

    Boiler plate steps Before Visual Studio decided to update

--- Source/GameMechs/Character/GM_PlayerController.cpp
+++ Source/GameMechs/Character/GM_PlayerController.cpp
@@ -2,4 +2,31 @@
 
 
 #include "GM_PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
+AGM_PlayerController::AGM_PlayerController()
+{
+	
+}
+
+void AGM_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(GameMappingContext);
+
+	TObjectPtr<UEnhancedInputLocalPlayerSubsystem> Subsystem = ULocalPlayer::
+		GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+
+	if (Subsystem)
+	{
+		Subsystem->AddMappingContext(GameMappingContext, 0);
+	}
+}
+
+
+void AGM_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+}
--- Source/GameMechs/Character/GM_PlayerController.h
+++ Source/GameMechs/Character/GM_PlayerController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "InputMappingContext.h"
 #include "GM_PlayerController.generated.h"
 
 /**
@@ -14,4 +15,15 @@ class GAMEMECHS_API AGM_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	AGM_PlayerController();
+	virtual void SetupInputComponent() override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> GameMappingContext;
 };
